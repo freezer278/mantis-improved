b5dc80afd1ef99040a752b910352f16346bb1aeb
Vladimir Morozov <freezer278@gmail.com>
2017-02-11 13:19:53 +0200

    небольшое изменение локализации

---
+++ config/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname               = 'webdev08.mysql.ukraine.com.ua';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'webdev08_mantis';
+$g_db_username            = 'webdev08_mantis';
+$g_db_password            = 'atbjge89';
+
+$g_default_timezone       = 'Europe/Kiev';
+
+$g_crypto_master_salt     = 'xbPT+hgsSzogDnex64eBbxmRZpiyGnr6qDs43GuIUbI=';
